267e61a4946c2df3b65e67b2dce8561b099ba185
Jayesh Manani <jsmanani@gmail.com>
2025-02-24 15:37:40 +0100

    clean config file addedd

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# config.py
+class Config:
+    MONGO_URI = f"Get the URL from your mongo db atlas account"
+    DB_NAME = "product_db"
+    CHROMA_DB_PATH = "./chroma_db"
+    IMAGE_COLLECTION = "products_images"
+    TEXT_COLLECTION = "products_text"
+    Collection_NAME = "products"
+    LOG_DB = "logs"
